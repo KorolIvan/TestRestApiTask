f261a3dbae28ae6454dea8081e9bb371b79c11ae
Ivan Korol <korol.vanja2008@gmail.com>
2020-05-25 00:18:25 +0200

    - allure report tool added to the framework

--- src/test/java/api/model/BaseCurrency.java
+++ src/test/java/api/model/BaseCurrency.java
@@ -1,6 +1,5 @@
 package api.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.Map;
@@ -10,7 +9,6 @@ import java.util.Map;
  */
 
 @Data
-@Builder
 public class BaseCurrency {
     private String base;
     private Map<String, Double> rates;
